0e920d1fbd39c7dbf0725eaa87f3d9cbaa9140e3
Veronica <veronicaegorova13@gmail.com>
2022-08-13 22:14:34 +0700

    XML parser

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,14 @@ import com.google.gson.Gson;
 import com.opencsv.CSVReader;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.HeaderColumnNameTranslateMappingStrategy;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -21,11 +24,15 @@ public class Main {
         columnMapping.put("country", "country");
         columnMapping.put("age", "age");
 
-        String fileName = "./data.csv";
+        String fileName = "data.csv";
         List<Employee> list = parseCSV(columnMapping, fileName);
 
         String json = listToJson(list);
-        writeString(json, "./data.json");
+        writeString(json, "data.json");
+
+        list = parseXML("data.xml");
+        json = listToJson(list);
+        writeString(json, "data2.json");
     }
 
     public static List<Employee> parseCSV(Map<String, String> column, String fileName) {
@@ -45,6 +52,42 @@ public class Main {
         }
 
         return new ArrayList<>();
+    }
+
+    public static List<Employee> parseXML(String fileName) {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        List<Employee> employees = new ArrayList<>();
+
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+
+            FileInputStream fis = new FileInputStream(fileName);
+            InputSource is = new InputSource(fis);
+            Document doc = builder.parse(is);
+
+            Element element = doc.getDocumentElement();
+            NodeList nodes = element.getChildNodes();
+
+            for (int i = 0; i < nodes.getLength(); i++) {
+                NodeList itemNode = nodes.item(i).getChildNodes();
+                if (itemNode.getLength() > 0) {
+                    Employee employee = new Employee();
+                    for (int j = 0; j < itemNode.getLength(); j++) {
+                        switch (itemNode.item(j).getNodeName()) {
+                            case "id" -> employee.id = Integer.parseInt(itemNode.item(j).getTextContent());
+                            case "firstName" -> employee.firstName = itemNode.item(j).getTextContent();
+                            case "lastName" -> employee.lastName = itemNode.item(j).getTextContent();
+                            case "country" -> employee.country = itemNode.item(j).getTextContent();
+                            case "age" -> employee.age = Integer.parseInt(itemNode.item(j).getTextContent());
+                        }
+                    }
+                    employees.add(employee);
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return employees;
     }
 
     public static String listToJson(List<Employee> list) {
